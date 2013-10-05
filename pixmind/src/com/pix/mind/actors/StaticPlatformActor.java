1c50d6737c0a1928e3587777d894dcf9fd89ecb1
Ale <Ale@192.168.15.7>
2013-10-05 12:45:34 +0200

    If a platform is not active, ignore collision.

--- pixmind/src/com/pix/mind/actors/StaticPlatformActor.java
+++ pixmind/src/com/pix/mind/actors/StaticPlatformActor.java
@@ -9,28 +9,30 @@ import com.pix.mind.box2d.bodies.StaticPlatform;
 
 public class StaticPlatformActor extends Image {
 	StaticPlatform staticPlatform;
-	boolean active = true;
-	
-	public StaticPlatformActor(StaticPlatform platform, Color color, boolean active){
+Color color;
+	public StaticPlatformActor(StaticPlatform platform, Color color,
+			boolean active) {
 		super(new Texture(Gdx.files.internal("data/textures/pixel.png")));
-		
-		staticPlatform = platform;
-		this.setSize(staticPlatform.PlatformWidth * PixMindGame.BOX_TO_WORLD * 2, staticPlatform.PlatformHeight * PixMindGame.BOX_TO_WORLD * 2);
-		this.setPosition(staticPlatform.getPosX(), staticPlatform.getPosY());
-		this.setColor(color);
-		if (!active){
-			this.setColor(color.r, color.g, color.b, 0.5f);
-			this.active = false;
-		}
-	}
 
-	public boolean isActive() {
-		return active;
+		staticPlatform = platform;
+		this.setSize(staticPlatform.PlatformWidth * PixMindGame.BOX_TO_WORLD
+				* 2, staticPlatform.PlatformHeight * PixMindGame.BOX_TO_WORLD
+				* 2);
+		this.setPosition(staticPlatform.getPosX(), staticPlatform.getPosY());
+		this.color = color;		
+		this.setActive(active);
+	
 	}
 
 	public void setActive(boolean active) {
-		this.active = active;
-	}
 
+		if (active) {
+			staticPlatform.fixture.setSensor(false);
+			this.setColor(color.r, color.g, color.b, 1);
+		} else {
+			staticPlatform.fixture.setSensor(true);
+			this.setColor(color.r, color.g, color.b, 0.2f);
+		}
+	}
 
 }
