332030ef7b0c75ccbe33c0612a1b3dbfb6d3aec6
Ale <Ale@192.168.15.7>
2013-10-05 12:49:15 +0200

    no changes

--- pixmind/src/com/pix/mind/actors/PlatformActivatorActor.java
+++ pixmind/src/com/pix/mind/actors/PlatformActivatorActor.java
@@ -1,32 +1,31 @@
 package com.pix.mind.actors;
 
-import com.badlogic.gdx.Gdx;
+
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.pix.mind.PixMindGame;
 import com.pix.mind.box2d.bodies.PlatformActivator;
-import com.pix.mind.box2d.bodies.StaticPlatform;
 
-public class PlatformActivatorActor extends Image {
+
+public class PlatformActivatorActor extends Actor {
 	
-	PlatformActivator platformActivator;
-	boolean active = true;
-	float radius;
+	private PlatformActivator platformActivator;
+	private boolean active = true;
+	private float radius;
 	
 	ShapeRenderer shapeRenderer;
 	public PlatformActivatorActor(PlatformActivator activator, Color color, boolean active){
-		super(new Texture(Gdx.files.internal("data/textures/pixel.png")));
+		//super(new Texture(Gdx.files.internal("data/textures/pixel.png")));
 		
 		platformActivator = activator;
 		this.radius = platformActivator.getActivatorRadius();
 		this.setPosition(platformActivator.getPosX(), platformActivator.getPosY());
 		this.setColor(color);
 		if (!active){
-			this.setColor(color.r, color.g, color.b, 0.5f);	
+			this.setColor(color.r, color.g, color.b, 0.1f);	
 			this.active = false;
 		}
 		shapeRenderer = new ShapeRenderer();
