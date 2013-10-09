da6f3a5005940b1180799af10b5983f2795ad104
archocron <archocron@gmail.com>
2013-10-09 14:51:16 +0200

    remove unused imports

--- pixmind/src/com/pix/mind/controllers/ArrowController.java
+++ pixmind/src/com/pix/mind/controllers/ArrowController.java
@@ -1,10 +1,8 @@
-package com.pix.mind.controllers;
+ package com.pix.mind.controllers;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.pix.mind.PixMindGame;
 import com.pix.mind.box2d.bodies.PixGuy;
