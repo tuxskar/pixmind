afef73a5f9992b17679a32a588560318b6ec4baf
ossdemura <juannsj@gmail.com>
2013-10-12 11:11:32 +0200

    new branch, replacing levelMainClassIssue

---
+++ pixmind/src/com/pix/mind/levels/PixGuyLevel.java
@@ -0,0 +1,12 @@
+package com.pix.mind.levels;
+
+import com.pix.mind.PixMindGame;
+
+public class PixGuyLevel {
+	PixMindGame game;
+	
+	public PixGuyLevel(PixMindGame game){
+		this.game = game;
+	}
+
+}
