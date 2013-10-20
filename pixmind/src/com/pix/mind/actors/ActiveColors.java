349e1ac6d960a9224161d14a9cc11fb897f0cf7d
tuxskar <tuxskar@gmail.com>
2013-10-20 19:13:56 +0200

    Added the map for the active colors

--- pixmind/src/com/pix/mind/actors/ActiveColors.java
+++ pixmind/src/com/pix/mind/actors/ActiveColors.java
@@ -1,17 +1,29 @@
 package com.pix.mind.actors;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.pix.mind.PixMindGame;
 
 public class ActiveColors {
 	public ArrayList<ActiveColor> colors;
 	private final static int COLORMARGIN = 10;
 	private int nActivated;
+	private Stage guiStage;
+	private HashMap<Color, ActiveColor> colorToTexture;
+
 	public ActiveColors(Stage guiStage, int nColors) {
+		this.guiStage = guiStage;
+		colorToTexture = new HashMap<Color, ActiveColor>();
+		colorToTexture.put(Color.BLUE, new ActiveColor(Color.BLUE));
+		colorToTexture.put(Color.CLEAR, new ActiveColor(Color.CLEAR));
+		colorToTexture.put(Color.GREEN, new ActiveColor(Color.GREEN));
+		colorToTexture.put(Color.RED, new ActiveColor(Color.RED));
+		colorToTexture.put(Color.YELLOW, new ActiveColor(Color.YELLOW));
+		colorToTexture.put(Color.BLACK, new ActiveColor(Color.BLACK));
+
 		colors = new ArrayList<ActiveColor>(nColors);
 		for (int i = 1; i <= nColors; i++) {
 			ActiveColor actColor = new ActiveColor(Color.CLEAR);
@@ -25,10 +37,19 @@ public class ActiveColors {
 	}
 
 	public void newActive(Color color) {
+		ActiveColor actColor;
 		for (int i = colors.size() - 1; i > 0; i--) {
-			colors.get(i).setDrawable((Drawable) new ActiveColor(colors.get(i - 1).getColor()));
+			colorToTexture.get(colors.get(i - 1).getColor()).remove();
+			actColor = colorToTexture.get(color);
+			actColor.setPosition(PixMindGame.w - actColor.getWidth() * i
+					- COLORMARGIN * i, PixMindGame.h - actColor.getHeight()
+					- COLORMARGIN);
+			guiStage.addActor(actColor);
+			colors.set(i, colorToTexture.get(colors.get(i - 1).getColor()));
 		}
-		colors.get(0).setDrawable((Drawable) new ActiveColor(color));
+		colorToTexture.get(colors.get(0).getColor()).remove();
+		actColor = colorToTexture.get(color);
+		colors.set(0, actColor);
 		nActivated++;
 	}
 
@@ -42,28 +63,28 @@ public class ActiveColors {
 	}
 
 	public void deActivate(Color color) {
-		int indexActivate = alreadyActive(color);
-		if (indexActivate != -1) {
-			for(int i=indexActivate; i<colors.size()-1;i++){
-				colors.get(i).setDrawable(new ActiveColor(colors.get(i+1).getColor()));
-			}
-			colors.get(colors.size()-1).setDrawable((Drawable) new ActiveColor(Color.CLEAR));
-			nActivated--;
-		}
+//		int indexActivate = alreadyActive(color);
+//		if (indexActivate != -1) {
+//			for (int i = indexActivate; i < colors.size() - 1; i++) {
+//				colors.get(i).setColor(colors.get(i + 1).getColor());
+//			}
+//			colors.get(colors.size() - 1).setColor(Color.CLEAR);
+//			nActivated--;
+//		}
 	}
-	
-	public int getMaxColors(){
+
+	public int getMaxColors() {
 		return colors.size();
 	}
-	
-	public Color deActivateOlderColors(){
-		Color color = colors.get(colors.size()-1).getColor().cpy();
-		colors.get(colors.size()-1).setDrawable((Drawable) new ActiveColor(Color.CLEAR));
+
+	public Color deActivateOlderColors() {
+		Color color = colors.get(colors.size() - 1).getColor().cpy();
+//		colors.get(colors.size() - 1).setColor(Color.CLEAR);
 		nActivated--;
 		return color;
 	}
-	
-	public int getNActivesColors(){
+
+	public int getNActivesColors() {
 		return nActivated;
 	}
 }
