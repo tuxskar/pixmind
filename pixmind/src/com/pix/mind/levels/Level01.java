28fb26d3782fe0c7419dac3d15816a0d92e0ce8d
tuxskar <tuxskar@gmail.com>
2013-10-21 18:17:11 +0200

    Merged with new develop branch

--- pixmind/src/com/pix/mind/levels/Level01.java
+++ pixmind/src/com/pix/mind/levels/Level01.java
@@ -11,170 +11,148 @@ import com.pix.mind.world.PixMindWorldRenderer;
 
 public class Level01 extends PixMindLevel {
 
-	public String levelTitle = "LevelOne";
+    public String levelTitle = "LevelOne";
 
-	PixMindGame game;
-	private static final int nActiveColors = 3;
-	public Level01(PixMindGame game) {
-		super(game, 640, 480, 20, 1.7f, 2, nActiveColors);
-		this.game = game;
-		levelNumber = 1;
-		// TODO Auto-generated constructor stub
-	}
+    PixMindGame game;
+    private static final int nActiveColors = 2;
+    public Level01(PixMindGame game) {
+            super(game, 640, 480, 20, 1.7f, 2, nActiveColors);
+            this.game = game;
+            levelNumber = 1;
+            // TODO Auto-generated constructor stub
+    }
 
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
+    @Override
+    public void show() {
+            // TODO Auto-generated method stub
 
-		super.show();
-		super.setNextLevel(game.getLevel02());
-		super.setActiveLevel(this);
-		// CREANDO EL NIVEL
+            super.show();
+            super.setNextLevel(game.getLevel02());
+            super.setActiveLevel(this);
+            // CREANDO EL NIVEL
 
-		// platform Actors and Activator Actors List
+            // platform Actors and Activator Actors List
 
-		// Creating All Static Platforms
+            // Creating All Static Platforms
 
-		// Box2D platforms
-		StaticPlatform sPlatform = new StaticPlatform(box2D.getWorld(), 1.7f, 0.1f,
-				1f, 0.1f);
-		StaticPlatform s2Platform = new StaticPlatform(box2D.getWorld(), 4.7f, 1.1f,
-				1, 0.1f);
+            // Box2D platforms
+            StaticPlatform sPlatform = new StaticPlatform(box2D.getWorld(), 1.7f, 0.1f,
+                            1f, 0.1f);
+            StaticPlatform s2Platform = new StaticPlatform(box2D.getWorld(), 4.7f, 1.1f,
+                            1, 0.1f);
 
-		// Actor Platforms
-		StaticPlatformActor s1Skin = new StaticPlatformActor(sPlatform,
-				Color.BLACK, true);
-		StaticPlatformActor s2Skin = new StaticPlatformActor(s2Platform,
-				Color.RED, false);
-		
-		// Add platforms to Stage
+            // Actor Platforms
+            StaticPlatformActor s1Skin = new StaticPlatformActor(sPlatform,
+                            Color.BLACK, true);
+            StaticPlatformActor s2Skin = new StaticPlatformActor(s2Platform,
+                            Color.RED, false);
+            
+            // Add platforms to Stage
 
-		scene2D.getGroupStage().addActor(s1Skin);
-		scene2D.getGroupStage().addActor(s2Skin);
-	
+            scene2D.getGroupStage().addActor(s1Skin);
+            scene2D.getGroupStage().addActor(s2Skin);
+    
 
-		// Add to platform list
+            // Add to platform list
 
-		box2D.getPlatformList().add(s1Skin);
-		box2D.getPlatformList().add(s2Skin);
-		
+            box2D.getPlatformList().add(s1Skin);
+            box2D.getPlatformList().add(s2Skin);
+            
 
-		// Creating All Activator
+            // Creating All Activator
 
-		// Box2D Activator
+            // Box2D Activator
 
-		PlatformActivator pActivator = new PlatformActivator(box2D.getWorld(),
-				0.7f, 1.9f, 0.2f);
-		PlatformActivator p2Activator = new PlatformActivator(box2D.getWorld(),
-				8, 6, 0.2f);
-		PlatformActivator p3Activator = new PlatformActivator(box2D.getWorld(),
-				0, 2, 0.2f);
-		PlatformActivator p4Activator = new PlatformActivator(box2D.getWorld(),
-				2, 5, 0.2f);
-		PlatformActivator p5Activator = new PlatformActivator(box2D.getWorld(),
-				1, 2, 0.2f);
-		PlatformActivator p6Activator = new PlatformActivator(box2D.getWorld(),
-				-1, 2, 0.2f);
-		// Actor Activator
+            PlatformActivator pActivator = new PlatformActivator(box2D.getWorld(),
+                            0.7f, 1.9f, 0.2f);
+            PlatformActivator p2Activator = new PlatformActivator(box2D.getWorld(),
+                            5, 2.9f, 0.2f);
 
-		PlatformActivatorActor a1Skin = new PlatformActivatorActor(pActivator,
-				Color.RED, false);
-		PlatformActivatorActor a2Skin = new PlatformActivatorActor(p2Activator,
-				Color.BLACK, true);
-		PlatformActivatorActor a3Skin = new PlatformActivatorActor(p3Activator,
-				Color.GREEN, false);
-		PlatformActivatorActor a4Skin = new PlatformActivatorActor(p4Activator,
-				Color.BLUE, true);
-		PlatformActivatorActor a5Skin = new PlatformActivatorActor(p5Activator,
-				Color.BLUE, true);
-		PlatformActivatorActor a6Skin = new PlatformActivatorActor(p6Activator,
-				Color.MAGENTA, false);
+            // Actor Activator
 
-		// Add activators to Stage
-		
-		
-		scene2D.getGroupStage().addActor(a1Skin);
-		scene2D.getGroupStage().addActor(a2Skin);
-		scene2D.getGroupStage().addActor(a3Skin);
-		scene2D.getGroupStage().addActor(a4Skin);
-		scene2D.getGroupStage().addActor(a5Skin);
-		scene2D.getGroupStage().addActor(a6Skin);
-		// Add to activator list
+            PlatformActivatorActor a1Skin = new PlatformActivatorActor(pActivator,
+                            Color.RED, false);
+            PlatformActivatorActor a2Skin = new PlatformActivatorActor(p2Activator,
+                            Color.BLACK, true);
 
-		box2D.getActivatorList().add(a1Skin);
-		box2D.getActivatorList().add(a2Skin);
-		box2D.getActivatorList().add(a3Skin);
-		box2D.getActivatorList().add(a4Skin);
-		box2D.getActivatorList().add(a5Skin);
-		box2D.getActivatorList().add(a6Skin);
-		// add to stage the group of actors
+            // Add activators to Stage
+            
+            
+            scene2D.getGroupStage().addActor(a1Skin);
+            scene2D.getGroupStage().addActor(a2Skin);
+            // Add to activator list
 
-		// Active colors
+            box2D.getActivatorList().add(a1Skin);
+            box2D.getActivatorList().add(a2Skin);
+            
+            // add to stage the group of actors
 
-		// Rendering the game
-		//box2D.addActivatedColor(Color.BLUE);
-		worldRenderer = new PixMindWorldRenderer(scene2D, box2D, gui);
+            // Active colors
 
-	}
+            // Rendering the game
+            //box2D.addActivatedColor(Color.BLUE);
+            worldRenderer = new PixMindWorldRenderer(scene2D, box2D, gui);
 
-	@Override
-	public void render(float delta) {
-		super.render(delta);
+    }
 
-	}
+    @Override
+    public void render(float delta) {
+            super.render(delta);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pix.mind.levels.PixMindLevel#resize(int, int)
-	 */
-	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		super.resize(width, height);
-	}
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pix.mind.levels.PixMindLevel#hide()
-	 */
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		super.hide();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.pix.mind.levels.PixMindLevel#resize(int, int)
+     */
+    @Override
+    public void resize(int width, int height) {
+            // TODO Auto-generated method stub
+            super.resize(width, height);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pix.mind.levels.PixMindLevel#pause()
-	 */
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		super.pause();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.pix.mind.levels.PixMindLevel#hide()
+     */
+    @Override
+    public void hide() {
+            // TODO Auto-generated method stub
+            super.hide();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pix.mind.levels.PixMindLevel#resume()
-	 */
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		super.resume();
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.pix.mind.levels.PixMindLevel#pause()
+     */
+    @Override
+    public void pause() {
+            // TODO Auto-generated method stub
+            super.pause();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pix.mind.levels.PixMindLevel#dispose()
-	 */
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.pix.mind.levels.PixMindLevel#resume()
+     */
+    @Override
+    public void resume() {
+            // TODO Auto-generated method stub
+            super.resume();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.pix.mind.levels.PixMindLevel#dispose()
+     */
+    @Override
+    public void dispose() {
+            // TODO Auto-generated method stub
+    }
 
 }
