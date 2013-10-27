4bcacebe509d5ba8c2f86b07a861e9ab657962e3
tuxskar <tuxskar@gmail.com>
2013-10-27 13:36:55 +0100

    Fixed intermediate platform width to do not cheat the level

--- pixmind/src/com/pix/mind/levels/Level11.java
+++ pixmind/src/com/pix/mind/levels/Level11.java
@@ -11,7 +11,7 @@ public class Level11 extends PixMindLevel {
 	PixMindGame game;
 	private static final int nActiveColors = 1;
 	public Level11(PixMindGame game) {
-        super(game, 1280, 1024, 600, 14, 6, nActiveColors);		this.game = game;
+        super(game, 1280, 1024, 600, 14, 5, nActiveColors);		this.game = game;
 		levelNumber = 11;
 	}
 
@@ -19,7 +19,7 @@ public class Level11 extends PixMindLevel {
 
 		// Active colors		// Box2D platforms		// Add to platform list		// Black StaticPlatforms		box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 13, 4, platW, platH, Color.BLACK, true));		box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 13, 6, platW, platH, Color.BLACK, true));		box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 13, 8, platW, platH, Color.BLACK, true));		box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 10, 5, platW, platH, Color.BLACK, true));		box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 10, 7, platW, platH, Color.BLACK, true));		box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 10, 9, platW, platH, Color.BLACK, true));        box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 4, 5, platW, platH, Color.BLACK, true));        box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 2, 6, platW, platH, Color.BLACK, true));        box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 2, 8, platW, platH, Color.BLACK, true));
 
-		// Black StaticPlatforms		box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 6, 4, 3*platW/4, platH, Color.RED, false));        box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 0, 7, platW, platH, Color.GREEN, false));        box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 4, 9, platW/2, platH, Color.BLUE, false));
+		// Black StaticPlatforms		box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 6, 4, 5*platW/8, platH, Color.RED, true));        box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 0, 7, platW, platH, Color.GREEN, false));        box2D.getPlatformList().add(                new StaticPlatformActor(box2D.getWorld(), 4, 9, platW/2, platH, Color.BLUE, false));
 		// Creating All Activator		// Box2D Activator adding to activator list        box2D.getActivatorList().add(            new PlatformActivatorActor(box2D.getWorld(), 6, 8, Color.BLACK, true));
         box2D.getActivatorList().add(            new PlatformActivatorActor(box2D.getWorld(), 2, 8.5f, Color.BLUE, false));
         box2D.getActivatorList().add(            new PlatformActivatorActor(box2D.getWorld(), 13, 9, Color.RED, false));
@@ -33,55 +33,30 @@ public class Level11 extends PixMindLevel {
 
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pix.mind.levels.PixMindLevel#resize(int, int)
-	 */
 	@Override
 	public void resize(int width, int height) {
 		// TODO Auto-generated method stub
 		super.resize(width, height);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pix.mind.levels.PixMindLevel#hide()
-	 */
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
 		super.hide();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pix.mind.levels.PixMindLevel#pause()
-	 */
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
 		super.pause();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pix.mind.levels.PixMindLevel#resume()
-	 */
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
 		super.resume();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pix.mind.levels.PixMindLevel#dispose()
-	 */
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
